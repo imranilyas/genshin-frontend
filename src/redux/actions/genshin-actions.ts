6f429550ec0e1a0aadd72758592b2a98b00f4f02
imranilyas <68575807+imranilyas@users.noreply.github.com>
2021-08-27 09:27:52 -0700

    Action Handlers
    not tested yet

--- src/redux/actions/genshin-actions.ts
+++ src/redux/actions/genshin-actions.ts
@@ -1,22 +1,70 @@
 import { Dispatch } from "redux";
 import axios from "../../../axiosConfig";
+import IItem from "../../entities/item";
+import { AppActions } from "./actions";
 
-const GetAllDrops = () => {
-
+const GetAllDrops = () => async (dispatch: Dispatch) => {
+    try {
+        // axios request
+        const res = await axios.get('/all');
+        dispatch({
+            type: AppActions.UPDATE_ITEM,
+            payload: {drops: res.data},
+        });
+    } catch (error) {
+        console.log('Error with GetAllDrops: ' + error);
+    }
 }
 
-const GetDrop = () => {
-
+const GetDropByName = (item: IItem) => async (dispatch: Dispatch) => {
+    try {
+        // axios request
+        const res = await axios.get(`/${item.dropName}`);
+        dispatch({
+            type: AppActions.UPDATE_ITEM,
+            payload: {drops: res.data},
+        });
+    } catch (error) {
+        console.log('Error Getting Drop By Name: ' + error);
+    }
 }
 
-const AddDrop = () => {
-
+const AddDrop = (item: IItem) => async (dispatch: Dispatch) => {
+    try {
+        // axios request
+        await axios.post('/', item);
+        const res = await axios.get('/all');
+        dispatch({
+            type: AppActions.UPDATE_ITEM,
+            payload: {drops: res.data},
+        });
+    } catch (error) {
+        console.log('Error with Adding an Item Drop: ' + error);
+    }
 }
 
-const UpdateDrop = () => {
-
+const UpdateDrop = (item: IItem) => async (dispatch: Dispatch) => {
+    try {
+        await axios.put('/', {...item});
+        const res = await axios.get('/all');
+        dispatch({
+            type: AppActions.UPDATE_ITEM,
+            payload: {drops: res.data},
+        });
+    } catch (error) {
+        console.log('Error with Updating a Drop: ' + error);
+    }
 }
 
-const DeleteDrop = () => {
-
+const DeleteDrop = (item: IItem) => async (dispatch: Dispatch) => {
+    try {
+        await axios.delete(`/${item.dropName}`);
+        const res = await axios.get('/all');
+        dispatch({
+            type: AppActions.UPDATE_ITEM,
+            payload: {drops: res.data},
+        });
+    } catch (error) {
+        console.log('Error with Deleting an Item Drop: ' + error);
+    }
 }
