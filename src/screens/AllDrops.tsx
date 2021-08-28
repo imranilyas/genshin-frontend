89f265d6d88f5e238f5f2e6d216175e4b73e2cc6
imranilyas <68575807+imranilyas@users.noreply.github.com>
2021-08-28 13:01:04 -0700

    Navigate to Add Drop Screen

--- src/screens/AllDrops.tsx
+++ src/screens/AllDrops.tsx
@@ -1,10 +1,24 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { View, FlatList, StyleSheet, Text } from "react-native";
+import { View, FlatList, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { RootStackParamList } from "../navigation/stackTypes";
 
 const AllDrops: React.FC = () => {
+    type main = StackNavigationProp<RootStackParamList, 'AllDrops'>
+    const navigation = useNavigation<main>();
+
+    //! to be removed
+    const add = () => {
+        navigation.navigate('AddDrop');
+    }
+    
     return (
         <View style = {styles.container}>
             <Text>All Drops Screen</Text>
+            <TouchableOpacity style = {styles.buttons} onPress={() => navigation.navigate('AddDrop')}>
+                <Text>Add Item Drop</Text>
+            </TouchableOpacity>
         </View>
     )
 }
@@ -15,6 +29,9 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     list: {},
+    buttons: {
+        backgroundColor: 'black',
+    },
 })
 
 export default AllDrops;
