31f3919c57a57f4b3870b00f54a4d6808bc478ae
imranilyas <68575807+imranilyas@users.noreply.github.com>
2021-08-29 11:14:31 -0700

    Deleting Works now

--- src/screens/ViewDrop.tsx
+++ src/screens/ViewDrop.tsx
@@ -4,29 +4,42 @@ import IItem from "../entities/item";
 import { useDispatch } from 'react-redux';
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParamList } from "../navigation/stackTypes";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import { deleteDrop } from "../redux/actions/genshin-actions";
 
 interface IProps {
     item: IItem,
 }
 
-const ViewDrop: React.FC<IProps> = (props: IProps) => {
+const ViewDrop: React.FC = () => {
     // Navigation
     type main = StackNavigationProp<RootStackParamList, 'AllDrops'>
     const navigation = useNavigation<main>(); 
-    const dispatch = useDispatch();   
+    const dispatch = useDispatch();
+    
+    // Route params
+    const route = useRoute();
+    const params = route.params as IItem;
 
     //Delete handler
     const deleteItem = () => {
-        dispatch(deleteDrop(props.item));
+        console.log(params.dropName);
+        dispatch(deleteDrop(params));
+        navigation.goBack();
     }
 
     return (
         <View>
             <Text>View Specific Drop Screen</Text>
-            <TouchableOpacity style = {styles.button} onPress = {deleteItem}>
-                <Text style = {styles.buttonText}>Delete Drop</Text>    
+            
+            {/* Edit Button */}
+            <TouchableOpacity style = {styles.blueBtn} onPress = {() => navigation.navigate('EditDrop', params)}>
+                <Text style = {styles.blueBtnText}>Edit Drop</Text>    
+            </TouchableOpacity>
+
+            {/* Delete Button */}
+            <TouchableOpacity style = {styles.redBtn} onPress = {deleteItem}>
+                <Text style = {styles.redBtnText}>Delete Drop</Text>    
             </TouchableOpacity>
         </View>
     )
@@ -38,15 +51,26 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
 
-    button: {
+    redBtn: {
         borderColor: 'red',
         borderWidth: 2,
     },
 
-    buttonText: {
+    blueBtn: {
+        borderColor: 'blue',
+        borderWidth: 2,
+    },
+
+    redBtnText: {
         textAlign: 'center',
         fontSize: 30,
         color: 'red',
+    },
+
+    blueBtnText: {
+        textAlign: 'center',
+        fontSize: 30,
+        color: 'blue',
     },
 })
 
