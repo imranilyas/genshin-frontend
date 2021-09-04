ca6d021f4c5d9b4aad8115cc2047d90238383c70
imranilyas <68575807+imranilyas@users.noreply.github.com>
2021-09-03 20:57:41 -0700

    Update Header.tsx
    potentially putting in searchbar

--- src/components/AllDrops/Header.tsx
+++ src/components/AllDrops/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Image, TouchableOpacity, View, Text, StyleSheet} from 'react-native';
+import {Image, TouchableOpacity, View, Text, StyleSheet, TextInput} from 'react-native';
 import { useNavigation } from "@react-navigation/native";
 import { RootStackParamList } from "../../navigation/stackTypes";
 import { StackNavigationProp } from "@react-navigation/stack";
@@ -8,6 +8,10 @@ const Header: React.FC = () => {
     // Navigation
     type main = StackNavigationProp<RootStackParamList, 'AllDrops'>
     const navigation = useNavigation<main>();
+
+    const search = () => {
+        // Searches for the drop using .filter or .includes
+    }
 
     return(
         <View>
@@ -18,6 +22,12 @@ const Header: React.FC = () => {
             <TouchableOpacity style = {styles.buttons} onPress={() => navigation.navigate('AddDrop')}>
                 <Text style = {styles.btnText}>Add Item Drop</Text>
             </TouchableOpacity>
+            {/* <TextInput style = {styles.searchbar} 
+                placeholder = "Search Item Drops"
+                placeholderTextColor = "white"
+                textAlign = 'center'
+                onSubmitEditing = {search}
+            /> */}
         </View>
     )
 }
@@ -40,6 +50,18 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         fontSize: 22,
         color: 'white',
-    }
+    },
+
+    searchbar: {
+        backgroundColor: '#01CDD9',
+        opacity: 0.8,
+        borderRadius: 1000/2,
+        marginBottom: '2%',
+        marginHorizontal: '2%',
+        fontSize: 22,
+        paddingHorizontal: '5%',
+        paddingVertical: '2%',
+        color: 'white',
+    },
 })
 export default Header;
